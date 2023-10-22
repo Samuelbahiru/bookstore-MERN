7a87139c765d97c59e66567af02453322b38acc3
samuel <samuelbahiru93@gmail.com>
2023-10-22 07:22:09 -0300

    author crud has been finished

--- routes/author.js
+++ routes/author.js
@@ -3,8 +3,22 @@ const router = express.Router()
 const Author = require('../models/author')
 // All Authors Routes
 
-router.get('/', (request, res)=>{
-    res.render('authors/index')
+router.get('/', async(req, res)=>{
+let searchOptions = {}
+ if(req.query.name != null && req.query.name !== ''){
+    searchOptions.name = new RegExp(req.query.name)
+}
+
+    try{
+        console.log("Searchoptions", searchOptions.name)
+        const authors = await Author.find(searchOptions)
+        console.log("authors", authors)
+        res.render('authors/index', {authors: authors, searchOptions: req.query })
+    }
+    catch{
+        res.redirect('/')
+    }
+   
 })
 
 
@@ -17,11 +31,34 @@ router.get('/new', (request, res)=>{
 
 // creating author
 
-router.post("/", (req, res)=>{
+router.post("/", async(req, res)=>{
     const author = new Author({
         name: req.body.name
     })
-    res.send(req.body.name)
+    try{
+        const newAuthor = await author.save()    //this is beacuse everything in mongodb is done asynchronusly
+        //  res.redirect(`authors/${newAuthor.id}`)
+            res.redirect('authors')
+    }
+    catch{
+        res.render('authors/new', {
+            author: author,
+            errorMessage: 'Error creating the Author'
+        })
+    }
+    // author.save().then((err, newAuthor)=>{
+    //     if(err){
+    //         res.render('authors/new', {
+    //             author: author,
+    //             errorMessage: 'Error has happend'
+    //         })
+    //     }
+    //     else{
+    //         // res.redirect(`authors/${newAuthor.id}`)
+    //         res.redirect(`authors`)
+    //     }
+    // })
+ 
 })
 
 
