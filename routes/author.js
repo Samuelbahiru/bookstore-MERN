ac5da3a3ecf2140d8d9cd17c36cebec20397f7e3
samuelbahiru <samuelbahiru93@gmail.com>
2023-11-18 18:20:42 +0300

    update author crud

--- routes/author.js
+++ routes/author.js
@@ -1,65 +1,77 @@
-const express = require ('express')
-const router = express.Router()
-const Author = require('../models/author')
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
 // All Authors Routes
 
-router.get('/', async(req, res)=>{
-let searchOptions = {}
- if(req.query.name != null && req.query.name !== ''){
-    searchOptions.name = new RegExp(req.query.name)
-}
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name);
+  }
 
-    try{
-        console.log("Searchoptions", searchOptions.name)
-        const authors = await Author.find(searchOptions)
-        console.log("authors", authors)
-        res.render('authors/index', {authors: authors, searchOptions: req.query })
-    }
-    catch{
-        res.redirect('/')
-    }
-   
-})
-
-
+  try {
+    console.log("Searchoptions", searchOptions.name);
+    const authors = await Author.find(searchOptions);
+    console.log("authors", authors);
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect("/");
+  }
+});
 
 // New Auhtor Role
 
-router.get('/new', (request, res)=>{
-    res.render('authors/new', {author: new Author()})
-})
+router.get("/new", (request, res) => {
+  res.render("authors/new", { author: new Author() });
+});
 
 // creating author
 
-router.post("/", async(req, res)=>{
-    const author = new Author({
-        name: req.body.name
-    })
-    try{
-        const newAuthor = await author.save()    //this is beacuse everything in mongodb is done asynchronusly
-        //  res.redirect(`authors/${newAuthor.id}`)
-            res.redirect('authors')
-    }
-    catch{
-        res.render('authors/new', {
-            author: author,
-            errorMessage: 'Error creating the Author'
-        })
-    }
-    // author.save().then((err, newAuthor)=>{
-    //     if(err){
-    //         res.render('authors/new', {
-    //             author: author,
-    //             errorMessage: 'Error has happend'
-    //         })
-    //     }
-    //     else{
-    //         // res.redirect(`authors/${newAuthor.id}`)
-    //         res.redirect(`authors`)
-    //     }
-    // })
- 
-})
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  try {
+    const newAuthor = await author.save(); //this is beacuse everything in mongodb is done asynchronusly
+    //  res.redirect(`authors/${newAuthor.id}`)
+    res.redirect("authors");
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating the Author",
+    });
+  }
+  // author.save().then((err, newAuthor)=>{
+  //     if(err){
+  //         res.render('authors/new', {
+  //             author: author,
+  //             errorMessage: 'Error has happend'
+  //         })
+  //     }
+  //     else{
+  //         // res.redirect(`authors/${newAuthor.id}`)
+  //         res.redirect(`authors`)
+  //     }
+  // })
+});
 
+router.get("/:id", (req, res) => {
+  //   res.send("show author", req.params.id);
+  res.status(200).send(req.params.id.toString());
+});
 
-module.exports = router
+router.get("/:id/edit", (req, res) => {
+  res.send("edit author", req.params.id);
+  res.status(200).send("edit", req.params.id.toString());
+});
+
+router.put("/:id", (req, res) => {
+  res.send("update author", req.params.id);
+  res.status(200).send("update", req.params.id.toString());
+});
+
+router.delete("./:id", (req, res) => {
+  res.send("delete author", req.params.id);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,47 +1,42 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const expressLayouts = require('express-ejs-layouts')
-const bodyParser = require('body-parser')
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/author')
-const bookRouter = require('./routes/book')
+const express = require("express");
+const mongoose = require("mongoose");
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/author");
+const bookRouter = require("./routes/book");
+const methodOverride = require("method-override");
 
-
-if(process.env.NODE_ENV !== "production") {
-    require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
+const app = express();
 
-const app = express()
-
-
-app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 // view configuration to use ejs
-app.set("view engine", "ejs")    /// this is we are setting which engine is we are gone use for our views
-app.set('views', __dirname + '/views')   /// this is settings the path where all of our views will be stored
-
+app.set("view engine", "ejs"); /// this is we are setting which engine is we are gone use for our views
+app.set("views", __dirname + "/views"); /// this is settings the path where all of our views will be stored
 
 // configration of layout(base html files and public files)
-app.set('layout', 'layouts/layout')    // this is saying that our base html or layout is find to layouts/layout
-app.use(expressLayouts)
-app.use(express.static('public'))   // telling the expresss that where we put our public files
-
+app.set("layout", "layouts/layout"); // this is saying that our base html or layout is find to layouts/layout
+app.use(expressLayouts);
+app.use(express.static("public")); // telling the expresss that where we put our public files
+app.use(methodOverride("_method"));
 
 //database configuration
 
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', (error)=>console.log(error))
-db.on('open', ()=>console.log("connected to mongo"))
-
-
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.on("open", () => console.log("connected to mongo"));
 
 // server configuration
-app.listen(process.env.PORT || 3000)
+app.listen(process.env.PORT || 3000);
 
-// routing 
- 
-app.use('/', indexRouter)
-app.use('/authors', authorRouter)
-app.use('/books', bookRouter )
+// routing
+
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
